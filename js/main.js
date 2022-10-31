5f4c1516696af73963367a52f24ed6d7054b3bbc
Wesley <matthew282@msn.com>
2022-10-30 17:13:50 -0700

    add checkWin()

--- js/main.js
+++ js/main.js
@@ -1,11 +1,34 @@
 function getComputerChoice() {
   array = ['Rock', 'Paper', 'Scissors']
-  return randomElement = array[Math.floor(Math.random() * array.length)];
+  return randomElement = array[Math.floor(Math.random() * array.length)].toUpperCase();
 }
 
 function getPlayerChoice() {
-  const choice = prompt('Enter "paper", "rock", or "scissors"');
+  const choice = prompt('Enter "paper", "rock", or "scissors"').toUpperCase();
   return choice;
 }
 
-console.log(getPlayerChoice())
+function checkWin(computerSelection, playerSelection) {
+  if (computerSelection == 'ROCK' && playerSelection == 'SCISSORS') {
+    return alert('You Lose! rock beats paper!');
+  } else if (computerSelection == 'ROCK' && playerSelection == 'PAPER') {
+    alert('You win! Paper beats rock!');
+  } else if (computerSelection == 'ROCK' && playerSelection == 'ROCK') {
+    alert('Draw!');
+  } else if (computerSelection == 'SCISSORS' && playerSelection == 'SCISSORS') {
+    alert('Draw!');
+  } else if (computerSelection == 'SCISSORS' && playerSelection == 'ROCK') {
+    alert('You win! Rock beats scissors!');
+  } else if (computerSelection == 'SCISSORS' && playerSelection == 'PAPER') {
+    alert('You lose! Scissors beat paper!');
+  } else if (computerSelection == 'PAPER' && playerSelection == 'PAPER') {
+    alert('Draw!');
+  } else if (computerSelection == 'PAPER' && playerSelection == 'ROCK') {
+    alert('You win! Paper beats rock!');
+  } else if (computerSelection == 'PAPER' && playerSelection == 'SCISSORS') {
+    alert('You lose! Scissors beats paper!');
+  } 
+}
+ 
+
+console.log(checkWin(getComputerChoice(), getPlayerChoice()))
