17db6dc61234af113a7f2e5257954724130e55a6
wesley <wesley@penguin>
2023-02-26 20:07:17 -0700

    add main() with nested event listeners for rock, paper, scissors divs)

--- js/main.js
+++ js/main.js
@@ -1,12 +1,12 @@
 const userScore = 0;
 const computerScore = 0;
 const userScore_span = document.getElementById("user-score");
-const computerScore_span = docuement.getElementById("computer-score");
+const computerScore_span = document.getElementById("computer-score");
 const scoreBoard_div = document.querySelector(".score-board");
 const result_div = document.querySelector(".result");
 const rock_div = document.getElementById("r");
 const paper_div = document.getElementById("p");
-const scissors_div = document.getElementByID("s");
+const scissors_div = document.getElementById("s");
 
 function getComputerChoice() {
   const choices = ['r', 'p', 's'];
@@ -14,8 +14,22 @@ function getComputerChoice() {
   return choices(randomNumber);
 }
 
+function main() {
+  rock_div.addEventListener('click', function() {
+    game("r");
+  });
+
+  paper_div.addEventListener('click', function() {
+    game("p");
+  });
+
+  paper_div.addEventListener('click', function() {
+    game("s");
+  }
+};
 
 
+main();
 
 
 
